91b3f5232bcf10847b9a5cf48b76150bcff7d8e4
Thiago Barros <thigb7@gmail.com>
2020-11-15 20:12:11 -0300

    :sparkles: Add: Criando a classe  memória

---
+++ Modulos/Memoria/modelo_memoria.py
@@ -0,0 +1,39 @@
+class Memoria:
+    def __init__(self, tamanho_memoria):
+        self.memoria = [0]*tamanho_memoria
+        self.offset_usuario = 64
+        self.offset_nucleo = 0
+
+
+    def verifica_memoria(self, offset, tamanho_processo, nucleo=0):
+        if(nucleo and (tamanho_processo+offset >= 64)):
+            return(0)
+        else:
+            for index in range(offset, tamanho_processo+offset):
+                if self.memoria[index] != 0:
+                    return(0)
+        return(1)
+    
+    def aloca_processo(self, offset, tamanho_processo, PID):
+        for index in range(offset, tamanho_processo+offset):
+            self.memoria[index] = PID
+
+    def aloca_processo_usuario(self, tamanho_processo, PID):
+        if self.verifica_memoria(self.offset_usuario, tamanho_processo):
+            self.aloca_processo(self.offset_usuario, tamanho_processo, PID)
+            self.offset_usuario = self.offset_usuario+tamanho_processo
+        else:
+            print('Não foi possivel alocar o processo em memória')
+            return(0)
+        return(1)
+
+    def aloca_processo_nucleo(self, tamanho_processo, PID):
+        if self.verifica_memoria(self.offset_nucleo, tamanho_processo, 1):
+            self.aloca_processo(self.offset_nucleo, tamanho_processo, PID)
+            self.offset_nucleo = self.offset_nucleo+tamanho_processo
+
+        else:
+            print('Não foi possivel alocar o processo em memória')
+            return(0)
+        return(1)
+    
