7891b67051a701bd4412fbc4118ec7fb93bee45b
Thiago Barros <thigb7@gmail.com>
2020-11-17 21:10:34 -0300

    :sparkles: :recycle: Add: Criando função remove_processo e refatorando função insere_processo

--- Modulos/Filas/modelo_fila.py
+++ Modulos/Filas/modelo_fila.py
@@ -8,19 +8,40 @@ class Fila:
 
     def insere_processo(self, processo):
         if((processo.prioridade == 0) and (len(self.fila_tempo_real) <= 1000)):
-            fila_tempo_real.append(processo)
+            self.fila_tempo_real.append(processo)
         elif((processo.prioridade == 1) and (len(self.fila_usuario["prioridade1"]) <= 1000)):
-            fila_usuario["prioridade1"].append(processo)
+            self.fila_usuario["prioridade1"].append(processo)
         elif((processo.prioridade == 2) and (len(self.fila_usuario["prioridade2"]) <= 1000)):
-            fila_usuario["prioridade2"].append(processo)
+            self.fila_usuario["prioridade2"].append(processo)
         elif((processo.prioridade == 3) and (len(self.fila_usuario["prioridade3"]) <= 1000)):
-            fila_usuario["prioridade3"].append(processo)
+            self.fila_usuario["prioridade3"].append(processo)
         else:
             print("Não há espaço na fila.")
             return(0)
         return(1)
     
-    def atualiza_prioridades(self):
+    def __atualiza_prioridades(self):
         self.fila_usuario['prioridade1'].extend(self.fila_usuario['prioridade2'])
         self.fila_usuario['prioridade2'] = self.fila_usuario['prioridade3']
         self.fila_usuario['prioridade3'] = []
+
+    def remove_processo(self):
+        if(len(self.fila_tempo_real) > 0):
+            processo = self.fila_tempo_real.pop(0)
+            self.__atualiza_prioridades()
+            return(processo)
+        elif(len(self.fila_usuario['prioridade1']) > 0):
+            processo = self.fila_usuario['prioridade1'].pop(0)
+            self.__atualiza_prioridades()
+            return(processo)
+        elif(len(self.fila_usuario['prioridade2']) > 0):
+            processo = self.fila_usuario['prioridade2'].pop(0)
+            self.__atualiza_prioridades()
+            return(processo)
+        elif(len(self.fila_usuario['prioridade3']) > 0):
+            processo = self.fila_usuario['prioridade3'].pop(0)
+            self.__atualiza_prioridades()
+            return(processo)
+        else:
+            print('Não há processos na fila.')
+            return(0)
