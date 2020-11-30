224118e7dd184642a491615930c4aee08ad36c34
Thiago Barros <thigb7@gmail.com>
2020-11-29 23:57:54 -0300

    :recycle: Refactor: Refatorando função insere_arquivos_inciais passando nos testes, executa operações está instável

--- Modulos/Arquivos/classe_disco.py
+++ Modulos/Arquivos/classe_disco.py
@@ -1,19 +1,20 @@
 class Disco:
     def __init__(self, arquivos_lidos):
-        blocos = [0]*arquivos_lidos.blocos_disco
-        arquivos = arquivos_lidos.arquivos
-        operacoes = arquivos_lidos.operacoes
+        self.blocos = [0]*arquivos_lidos['blocos_disco']
+        self.arquivos = arquivos_lidos['arquivos']
+        self.operacoes = arquivos_lidos['operacoes']
 
     def insere_arquivos_iniciais(self):
         for arquivo in self.arquivos:
-            for i in range(arquivo.inicio, arquivo.tamanho):
+            for i in range(arquivo.inicio, arquivo.inicio+arquivo.tamanho):
                 self.blocos[i] = arquivo.nome
+        return 1
 
     def __busca_espaco_vazio(self, tamanho_arquivo):
         espaco_contiguo_vazio = 0
         espacos_vazios = 0
-        for i in range(len(self.bloco)):
-            if not self.bloco[i]:
+        for i in range(len(self.blocos)):
+            if not self.blocos[i]:
                 espacos_vazios += 1
                 if espacos_vazios > espaco_contiguo_vazio:
                     espaco_contiguo_vazio = espacos_vazios
@@ -27,8 +28,8 @@ class Disco:
     def __busca_arquivo(self, nome):
         tamanho_arquivo = 0
         primeira_ocorrencia = 0
-        for i in range(len(self.bloco)):
-            if(self.bloco[i] == nome):
+        for i in range(len(self.blocos)):
+            if(self.blocos[i] == nome):
                 if not primeira_ocorrencia:
                     inicio_arquivo = i
                     primeira_ocorrencia = 1
@@ -36,36 +37,36 @@ class Disco:
         return(primeira_ocorrencia, inicio_arquivo, tamanho_arquivo)
 
     def __operacao_inserir(self, operacao):
-        offset = __busca_espaco_vazio(operacao.tamanho)
+        offset = self.__busca_espaco_vazio(operacao.tamanho)
         if offset != -1:
             for i in range(operacao.tamanho):
-                self.bloco[i] = operacao.nome
+                self.blocos[i] = operacao.nome
             return (1)
         else:
             return (0)
 
     def __operacao_deletar(self, operacao):
-        arquivo_achado, inicio_arquivo, tamanho_arquivo = __busca_arquivo(operacao.nome)
+        arquivo_achado, inicio_arquivo, tamanho_arquivo = self.__busca_arquivo(operacao.nome)
         if not arquivo_achado:
             ("Arquivo inexistente!")
             return(0)
         for i in range(inicio_arquivo, tamanho_arquivo):
-            self.bloco[i] = 0
+            self.blocos[i] = 0
         return(1)
 
-    def __executa_operacao(operacao):
+    def __executa_operacao(self, operacao):
         if (not operacao.cod_operacao and not operacao.tamanho):
-            if __operacao_deletar(operacao):
+            if self.__operacao_deletar(operacao):
                 return({'status': 1, 'texto': 'O processo ' + operacao.id_processo + ' \
                     deletou o arquivo ' + operacao_nome + '.'})
             else:
                 return({'status': 0, 'texto': 'Arquivo inexistente!'})
         else:
-            offset = __operacao_inserir(operacao)
+            offset = self.__operacao_inserir(operacao)
             if offset:
                 texto_blocos = 'blocos'
                 for bloco_ocupado in range(offset, operacao.tamanho):
-                    texto_blocos += ', ' + bloco_ocupado
+                    texto_blocos += ', ' + str(bloco_ocupado)
                 return({'status': 1, 'texto': 'O processo ' + operacao.id_processo + ' \
                     criou o arquivo ' + operacao.nome + ' (' + texto_blocos + ').'})
             else:
@@ -75,5 +76,5 @@ class Disco:
     def executa_operacoes(self):
         status_operacoes = []
         for operacao in self.operacoes:
-            status_operacoes.append(__executa_operacao(operacao))
+            status_operacoes.append(self.__executa_operacao(operacao))
         return status_operacoes
--- Modulos/Arquivos/modelo_arquivo.py
+++ Modulos/Arquivos/modelo_arquivo.py
@@ -1,8 +1,8 @@
 class Arquivo:
     def __init__(self, dados_arquivo):
         self.nome = dados_arquivo[0]
-        self.inicio = dados_arquivo[1]
-        self.tamanho = dados_arquivo[2]
+        self.inicio = int(dados_arquivo[1])
+        self.tamanho = int(dados_arquivo[2])
 
 
 class OperacaoArquivo:
@@ -11,6 +11,6 @@ class OperacaoArquivo:
         self.cod_operacao = dados_operacao[1]
         self.nome = dados_operacao[2]
         if(len(dados_operacao) == 4):
-            self.tamanho = dados_operacao[3]
+            self.tamanho = int(dados_operacao[3])
         else:
             self.tamanho = 0
